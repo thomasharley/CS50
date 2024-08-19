5f2032181a5fda15a82b6ae7602190098238795c
Thomas Harley <thomas.harley.1510@gmail.com>
2024-08-19 23:11:50 +0100

    working on speller.c problem

--- week_5_data_structures/speller/dictionary.c
+++ week_5_data_structures/speller/dictionary.c
@@ -2,6 +2,10 @@
 
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "dictionary.h"
 
@@ -12,43 +16,102 @@ typedef struct node
     struct node *next;
 } node;
 
-// TODO: Choose number of buckets in hash table
-const unsigned int N = 26;
 
-// Hash table
-//node *table[N];
+// Hash table: Choose number of buckets in hash table
+node *table[65536]; /* enough buckets to represent 3 letter combinations*/
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
     // TODO
+    /* convert the word to lower case */
+    char lowerCase[LENGTH + 1];
+    int i = 0;
+    while( word[i] != '\0') {
+        lowerCase[i] = tolower(word[i]);
+        i++;
+    }
+    lowerCase[i] = '\0';
+
+    int hashCode = hash(lowerCase);
+    node *current = table[hashCode];
+    while(current != NULL) {
+        if(strcmp(current->word, lowerCase) == 0) {
+            return true;
+        }
+        current = current -> next;
+    }
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    /// TODO: Improve this hash function
+    unsigned int code = 0;
+    int i = 0;
+    while(word[i] != '\0'){
+        code = (code << 2) ^ word[i];
+        i++;
+    }
+    return code % 65536;
+    //return ((toupper(word[0]) - 'A') * 10000) + ((toupper(word[1]) - 'A') * 100) + ((toupper(word[2]) - 'A') * 1);
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    return false;
+
+    FILE *loaded_dict = fopen(dictionary, "r");
+    if(loaded_dict == NULL) {
+        return false;
+    } 
+
+    char word[LENGTH+1];
+    while(fscanf(loaded_dict, "%s", word) != EOF) {
+
+        node *new = malloc(sizeof(node));
+        if(new == NULL) {
+            fclose(loaded_dict);
+            return false;
+        }
+
+        strcpy(new->word, word);
+        new->next = table[hash(word)];
+        table[hash(word)] = new;
+    }
+
+    fclose(loaded_dict);
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    
+    int count = 0;
+    for(int i = 0; i < 65536; i++){
+        node *current = table[i];
+        while(current != NULL){
+            count++;
+            current = current->next;
+        }
+    }
+    return count;
+    
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
 bool unload(void)
 {
     // TODO
-    return false;
+    for(int i = 0; i < 65536; i++) {
+        node *current = table[i];
+        while(current != NULL) {
+            node *temp = current;
+            current = current->next;
+            free(temp);
+        }
+    }
+    return true;
 }
